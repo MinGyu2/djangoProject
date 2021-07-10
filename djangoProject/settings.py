6eff1e648f8f98d2da6f0ff47572a7d4750eb9ca
MinGyu2 <mq04034@naver.com>
2021-07-11 04:24:25 +0900

    add djangosetting

--- djangoProject/settings.py
+++ djangoProject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-xmf6t@y2l=$u!nnr6c%d7b&k6keunztk!a7n6f503zmqel-so)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.0.26']
+ALLOWED_HOSTS = ['192.168.0.26', '127.0.0.1']
 
 
 # Application definition
