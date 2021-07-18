4f9ecae1d21b134ed1abfd4f066253148385ae90
MinGyu2 <mq04034@naver.com>
2021-07-19 05:19:25 +0900

    postgresql error table 추가

--- apps/models.py
+++ apps/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+# db table make
+class Error(models.Model):
+    equip_name = models.CharField(max_length=30)
+    error_content = models.TextField()
